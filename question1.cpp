47ba0f8d464eb8d39f81cc37141f0477dcbbf6c7
JP10JOT <97289379+JapjotS@users.noreply.github.com>
2025-12-01 09:40:37 +0000

    Add part_two calculation for movement logic
    Introduces a new variable 'part_two' to track additional movement logic for both 'L' and 'R' directions. Updates the output to display both 'zero_times' and 'part_two'.

--- question1.cpp
+++ question1.cpp
@@ -10,6 +10,7 @@ int main(){
     const int max = 100;
     int start = 50;
     int zero_times = 0;
+    int part_two = 0;
 
     string input_line;
     ifstream input_file("C:\\CODING\\aoc-2025\\question1input.txt");
@@ -27,17 +28,25 @@ int main(){
 
             char dir = matches[1].str()[0];
             int dist = stoi(matches[2].str());
-            dist %= 100; //cause the other thing isnt fricking working this is a piece of shit
+            //dist %= 100; //cause the other thing isnt fricking working this is a piece of shit
 
             //cout << matches[2] << endl;
-            if (dir == 'L'){
-                //cout << start << endl;
-                //start = start - stoi(matches[2]);
-                //if (start < 0)
-                    start = (start - dist + 100) % 100;
-                //}
-            } else {
-                start = (start+dist)%100;
+            if (dir == 'L') {
+                int S = start;
+                int D = dist;
+
+                if (S == 0) {
+                    part_two += D / 100;
+                } else if (D >= S) {
+                    part_two += 1 + (D - S) / 100;
+                }
+
+                start = (start - (dist % 100) + 100) % 100;
+            }
+
+            if (dir == 'R') {
+                part_two += (start + dist) / 100;
+                start = (start + dist % 100) % 100;
             }
 
             //if (matches[1] == 'R'){
@@ -60,7 +69,8 @@ int main(){
     }
 
     input_file.close();
-    cout << zero_times;
+    cout << zero_times << endl;
+    cout << part_two;
 
     return 0;
 
